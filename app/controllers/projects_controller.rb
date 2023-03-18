9fc4748205696d067387487cd617ad8ca94e4f39
LucaDamiano DiVittorio <luca.div@hotmail.com>
2023-03-18 15:31:25 +0000

    completed index page for projects added clickable card with truncated title and description created actions in the projects controller and styled the form to create a new project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,22 +1,51 @@
 class ProjectsController < ApplicationController
+  before_action :set_project, only: %i[show, edit, update, destroy]
+
   def index
+    @projects = Project.all
   end
 
   def show
+    @comment = Comment.new
   end
 
   def new
+    @project = Project.new
   end
 
   def create
+    @project = Project.new(project_params)
+    @project.user = current_user
+    if @project.save
+      redirect_to project_path(@project), notice: "Project was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
   end
 
   def update
+    if @project.update(project_params)
+      redirect_to @project, notice: "Project was successfully updated."
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @project.destroy
+    redirect_to projects_url, notice: "Project was successfully destroyed."
+  end
+
+  private
+
+  def set_project
+    @project = Project.find(params[:id])
+  end
+
+  def project_params
+    params.require(:project).permit(:name, :description, :status)
   end
 end
